98129aee3bfcdb40434e12ad93a443153caae5b3
Tim Smyr <godlyfast@gmail.com>
2019-02-22 00:46:52 +0200

    Updates

---
+++ precache-manifest.87cd82d72adf9995fdad76ca65dc3660.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7c5cdaed5203f388883e",
+    "url": "/static/js/main.7c5cdaed.chunk.js"
+  },
+  {
+    "revision": "1661d0e51e7ba47e2ce7",
+    "url": "/static/js/2.1661d0e5.chunk.js"
+  },
+  {
+    "revision": "dc174a11bd3a22c3721aaec381aad8c6",
+    "url": "/index.html"
+  }
+];
