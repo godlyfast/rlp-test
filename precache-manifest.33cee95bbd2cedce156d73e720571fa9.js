ff5a1986368a63f87f2655837c845bec3644b46d
Tim Smyr <godlyfast@gmail.com>
2019-02-22 00:53:32 +0200

    Updates

---
+++ precache-manifest.33cee95bbd2cedce156d73e720571fa9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d53337bdd76cb6eec6e8",
+    "url": "/rlp-test/static/js/runtime~main.d53337bd.js"
+  },
+  {
+    "revision": "39b1d8cf7782cafe6e0a",
+    "url": "/rlp-test/static/js/main.39b1d8cf.chunk.js"
+  },
+  {
+    "revision": "1661d0e51e7ba47e2ce7",
+    "url": "/rlp-test/static/js/2.1661d0e5.chunk.js"
+  },
+  {
+    "revision": "7d33a12f2b5976fa6a12eccc8b8bf0e3",
+    "url": "/rlp-test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87cd82d72adf9995fdad76ca65dc3660.js"
+  "/rlp-test/precache-manifest.33cee95bbd2cedce156d73e720571fa9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/rlp-test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
